bf538b6fc272d5b31fd89d46e2e9bcac1f447e03
AndriiMishch <anderito02@gmail.com>
2023-07-03 19:30:51 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+createGallery();
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionData: "alt",
+  captionDelay: 250,
+});
+function createGallery() {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" width="800" height="600"/>
+   </a>
+</li>`;
+    })
+    .join("");
+  galleryList.insertAdjacentHTML("afterbegin", markup);
+} 
